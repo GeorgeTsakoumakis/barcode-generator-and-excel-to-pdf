d231d7138724f5d74f3d265e42704eac86ae46fe
George Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2023-04-10 00:53:50 +0300

    Add code inside method and call from main.py

--- sequencer.py
+++ sequencer.py
@@ -1,4 +1,14 @@
-import main
+import single
 
-for i in range(3000, 3005):
-    main.main('ATL-00'+str(i))
+def sequencer():
+    start = int(input('Enter starting number: \n> '))
+    end = int(input('Enter ending number: \n> '))
+    filename, copy, cell = single.main()
+    for i in range(start, end+1):
+        data = 'ATL-00'+str(i)
+        single.barcode_generator(data)
+        single.pic_in_excel(data, filename, copy, cell)
+        single.xl_to_pdf(data, copy)
+
+if __name__ == '__main__':
+    sequencer()
